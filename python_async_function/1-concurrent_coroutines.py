ed52aa3f73eda880b91311fe00f3e75e2413150f
Alreix <alreixdev@gmail.com>
2026-04-09 14:41:43 +0200

    fix: import typing List for list typing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,7 @@
 """This module defines coroutine that runs multiple coroutines concurrently."""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
