37f40873a9fa8d0041337e91a67ff2aa34f36a0c
Alreix <alreixdev@gmail.com>
2026-04-09 14:38:25 +0200

    feat(1-concurrent): add a coroutine that runs multiple coroutines concurrently

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module defines coroutine that runs multiple coroutines concurrently."""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """Spawn wait_random n times and return the delays in ascending order."""
+    delays = []
+
+    coroutines = [wait_random(max_delay) for i in range(n)]
+
+    for coroutine in asyncio.as_completed(coroutines):
+        delay = await coroutine
+        delays.append(delay)
+
+    return delays
