8d4f635f3d66e03b727e89de649bbac6fa75a62f
Alreix <alreixdev@gmail.com>
2026-04-10 12:37:15 +0200

    feat: add coroutine that measure runtime of async comprehension

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This module defines coroutine that measures runtime of async comprehensio"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Execute async comprehensions in parralel and return total runtime"""
+    start = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.time()
+    return end - start
