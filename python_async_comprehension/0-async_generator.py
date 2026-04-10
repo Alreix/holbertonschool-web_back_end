0e095d6fabc7488138f8e51e3ff95dfc9e0eeae7
Alreix <alreixdev@gmail.com>
2026-04-10 16:35:52 +0200

    fix: add None paremeter for the generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random numbers asynchronously with a 1-second delay."""
     for i in range(10):
         await asyncio.sleep(1)
