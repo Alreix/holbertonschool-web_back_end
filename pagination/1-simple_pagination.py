af5641bf898dafc38dfe90bb611bd1bc4753da8d
Alreix <alreixdev@gmail.com>
2026-04-20 11:27:53 +0200

    feat(1-simple_pagination): add a simple pagination on a CSV dataset with the helper of the previous task

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This module provides simple pagination on a CSV dataset.
+"""
+
+import csv
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple containing start and end indexes for pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self) -> None:
+        """Initialize the server."""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Return the cached dataset loaded from the CSV file."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+        
+        return dataset[start_index:end_index]
