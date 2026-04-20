d1fd66071c868893705626c71cd3f8a6bb450433
Alreix <alreixdev@gmail.com>
2026-04-20 11:12:43 +0200

    feat(0-simple_helper): add an helper function for pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a helper function for pagination.
+"""
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple containing the start and end indexes for pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
