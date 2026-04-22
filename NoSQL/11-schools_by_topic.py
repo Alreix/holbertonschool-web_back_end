2b4a23c69c4f8f93f5ef540da91c7951d579e65f
Alreix <alreixdev@gmail.com>
2026-04-22 11:52:24 +0200

    fix: add the s to topics

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Return the list of schools having a specific topic."""
-    return list(mongo_collection.find({"topic": topic}))
+    return list(mongo_collection.find({"topics": topic}))
