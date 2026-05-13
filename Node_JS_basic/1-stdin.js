7a0fb9a18ae00ccabcb8f8767cc792d959d1cda6
Alreix <alreixdev@gmail.com>
2026-05-13 11:14:15 +0200

    fix: use tostring and remove utf8 verification

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name.toString()}`);
 });
 
 process.stdin.on('end', () => {
